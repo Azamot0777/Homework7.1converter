bbfb284c9ed161874b4143e7d53643847a7141fb
Heisenberg <Persey_98@list.ru>
2020-04-06 21:26:25 +0500

    Начало создания класса

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework7_1Converter
+{
+    class Converter
+    {
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
