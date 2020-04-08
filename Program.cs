b2deddc8556b5cd0f3c5a281319950946bbb9b2e
Heisenberg <Persey_98@list.ru>
2020-04-08 17:12:51 +0500

    Создал класс и конструктор

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace Homework7_1Converter
         public double usd { get; set; }
         public double euro { get; set; }
 
+        public double rubl { get; set; }
+
+        public Converter(double usd, double euro, double rubl)
+        {
+            this.usd = usd;
+            this.euro = euro;
+            this.rubl = rubl;
+        }
+        public SomoniToUsd(double somoni)
+        {
+            double usd = Math.Round(somoni * usd);
+            return usd;
+        }
     }
 
 
