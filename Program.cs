f247cf50a50db4c17858a921fa1c0b07bd04ec9a
Heisenberg <Persey_98@list.ru>
2020-04-08 17:33:52 +0500

    Методы конверторы

--- Program.cs
+++ Program.cs
@@ -9,16 +9,65 @@ namespace Homework7_1Converter
 
         public double rubl { get; set; }
 
-        public Converter(double usd, double euro, double rubl)
+        public Converter(double ust, double euroc, double rub)
         {
-            this.usd = usd;
-            this.euro = euro;
-            this.rubl = rubl;
+            this.usd = ust;
+            this.euro = euroc;
+            this.rubl = rub;
         }
-        public SomoniToUsd(double somoni)
+        public static double cSomoniToUsd(double somoni)
         {
             double usd = Math.Round(somoni * usd);
             return usd;
+        }
+        public static double cSomoniToEuro(double somoni)
+        {
+            double euro = Math.Round(somoni * euro);
+            return euro;
+        }
+        public static double cSomoniToRubl(double somoni)
+        {
+            double rubl = Math.Round(somoni * rubl);
+            return rubl;
+        }
+        public static double cUsdToSomoni(double us)
+        {
+            if (usd != 0)
+            {
+                double som = Math.Round(ust / usd);
+                return som;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка1");
+                return 0;
+            }
+        }
+        public static double cEuroToSomoni(double euroc)
+        {
+            if (euro != 0)
+            {
+                double som = Math.Round(euroc / euro);
+                return som;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка2");
+                return 0;
+            }
+        }
+        public static double cRublToSomoni(double rub)
+        {
+            if (rub != 0)
+            {
+                double som = Math.Round(rubl / rub, 4);
+                return som;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка3");
+                return 0;
+            }
         }
     }
 
